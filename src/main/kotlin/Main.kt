2558dbfccf617e8a872615e8a399aee035223634
pavel <gitkornienkov@gmail.com>
2023-09-02 23:43:08 +0300

    Add variables and classes

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,10 @@
 
 fun main() {
-    var post = Post(1,1,1,1,1,"Hello",1,1,false,Comments(),Likes())
-    var postCopy = Post(1,1,1,1,2,"Hello world",1,1,false,Comments(),Likes())
+    var post = Post(1,2,3,4,5,"Hello Kotlin", 25424,5345,true,Comments(),Likes(), Reports(), Views(),"post", Geo("Home", "gefgs", Place()),35435345, true, true, true, true)
+
     println(WallService.add(post))
     println(WallService.add(post))
     println(WallService.add(post))
     println(WallService.add(post))
-    println(WallService.update(postCopy))
     println(WallService.see(1))
 }
