fbed21b63c9cd1602ef59860f44216654ce332e5
Pavel Kornienkov <122346047+CapriKorP@users.noreply.github.com>
2023-09-06 20:00:07 +0300

    Fix tests

--- src/test/kotlin/online/kornienkov/classes/WallServiceTest.kt
+++ src/test/kotlin/online/kornienkov/classes/WallServiceTest.kt
@@ -1,8 +1,12 @@
 package online.kornienkov.classes
 
 import Comments
+import Geo
 import Likes
+import Place
 import Post
+import Reports
+import Views
 import WallService
 import org.junit.Test
 import org.junit.Assert.*
@@ -14,11 +18,9 @@ class WallServiceTest {
     fun clearBeforeTest() {
         WallService.clear()
     }
-
-    var post = Post(1,1,1,1,1,"Hello",1,1,false,Comments(),Likes())
-    var postCopy = Post(1,1,1,1,2,"Hello world",1,1,false,Comments(),Likes())
-    var postError = Post(2,1,1,1,2,"Hello world",1,1,false,Comments(),Likes())
-
+    var post = Post(1,2,3,4,5,"Hello Kotlin", 25424,5345,true,Comments(),Likes(), Reports(), Views(),"post", Geo("Home", "gefgs", Place()),35435345, true, true, true, true)
+    var postError = Post(2,2,3,4,5,"Hello Kotlin and Kotlin and Error", 25424,5345,true,Comments(),Likes(), Reports(), Views(),"post", Geo("Home", "gefgs", Place()),35435345, true, true, true, true)
+    var postCopy = Post(1,2,3,4,5,"Hello Kotlin and Kotlin", 25424,5345,true,Comments(),Likes(), Reports(), Views(),"post", Geo("Home", "gefgs", Place()),35435345, true, true, true, true)
     @Test
     fun add() {
         var actual = WallService.add(post).id
